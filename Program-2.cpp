fb4e6f61c4cdb24f59ff25118aa38833d98231a3
Rahul Bansal <61049580+rahulbansal3005@users.noreply.github.com>
2022-08-01 17:40:14 +0530

    Update Program-2.cpp

--- Program-2.cpp
+++ Program-2.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+vector<int> res;
+void solve(int n)
+{
+    int i=1;
+    for(int j=1;j<=n;j++)
+    {
+        res.push_back(i);
+        i+=2;
+    }
+}
+int main() {
+	int n;
+	cin>>n;
+	solve(n);
+	for(auto i:res)
+	cout<<i<<" ";
+	return 0;
+}
