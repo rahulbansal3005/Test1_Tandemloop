b3d119efac95a2101359a1d93e7338c5698f3ac3
Rahul Bansal <61049580+rahulbansal3005@users.noreply.github.com>
2022-08-01 17:35:59 +0530

    Update Program-1.cpp

--- Program-1.cpp
+++ Program-1.cpp
@@ -1,1 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+class Calculator{
+  double a;
+  double b;
+  string op;
+  public:
+    void setVar(double fir,double sec,string oper)
+    {
+        a=fir;
+        b=sec;
+        op=oper;
+    }
+    double getCalc()
+    {
+        if(op=="Addition")
+        return a+b;
+        if(op=="Subtraction")
+        return a-b;
+        if(op=="Multiplication")
+        return a*b;
+        if(op=="Division")
+        return a/b;
+    }
+  
+};
+int main() {
+	Calculator obj;
+	obj.setVar(5.66,5.23,"Addition");
+	cout<<obj.getCalc();
+  return 0;
+}
